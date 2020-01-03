9a41c9d95ec8bda327720b44d4d03a3b34527944
Leon Jamison <leonjamison@icloud.com>
2020-01-03 14:52:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,10 +7,23 @@ def directors_totals(nds)
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-    nds
-  result = {directors_database(directors_totals)
+  director_index = 0
+  totals = {}
 
-  }
+  while director_index < nds.length do
+    director_name = nds[director_index][:name]
+    totals[director_name] = 0
+    movie_index = 0
+
+    while movie_index < nds[director_index][:movies].length do
+      totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1
+    end
+
+    director_index += 1
+  end
+
+  totals
 
   #
   # Use loops, variables and the accessing method, [], to loop through the NDS
@@ -21,5 +34,4 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
 end
